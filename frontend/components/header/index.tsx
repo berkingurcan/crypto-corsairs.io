586caa84e99d395a5830f4d1130b5c08da199d8a
berkingurcan <berkingurcan@yandex.com>
2023-01-17 17:31:40 +0300

    added header

--- frontend/components/header/index.tsx
+++ frontend/components/header/index.tsx
@@ -1,10 +1,21 @@
+import Link from 'next/link';
 import React from 'react';
+import { FaAnchor } from 'react-icons/fa';
+import styles from './styles.module.css';
 
 function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <header className={`${styles.header} container fluid`}>
+      <div className={styles.headerWrapper}>
+        <Link href='/' className={styles.logo}>
+          <FaAnchor /> SOLPIRATES
+        </Link>
+        <nav className={styles.navigationMenu}>
+          <Link href='/course'>COURSES</Link>
+          <Link href='/about'>ABOUT SOLPIRATES</Link>
+        </nav>
+      </div>
+    </header>
   );
 }
 

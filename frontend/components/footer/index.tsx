b82fa782444f3942ba30b7e231bbf78aa0c85b22
berkingurcan <berkingurcan@yandex.com>
2023-01-18 14:52:00 +0300

    feat add footer

--- frontend/components/footer/index.tsx
+++ frontend/components/footer/index.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
+
+import styles from "./styles.module.css";
 
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
+    <footer className={styles.footer}>
+      Created by&nbsp;
+      <Link href="https://github.com/berkingurcan" target="_blank">
+        Berkin Gürcan
+      </Link>
+    </footer>
   );
 }
 

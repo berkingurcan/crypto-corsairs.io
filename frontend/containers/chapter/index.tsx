a5e1ac16562340afc86d04f43eca2f99b78e54ee
berkingurcan <berkingurcan@yandex.com>
2023-01-20 15:56:22 +0300

    get course

--- frontend/containers/chapter/index.tsx
+++ frontend/containers/chapter/index.tsx
@@ -4,14 +4,30 @@ import Editor from '@/components/editor';
 import DiffEditor from '@/components/diff-editor';
 import Sidebar from '@/components/sidebar';
 
-function Chapter({course_id, chapter_id}: any) {
+async function getCourse(id: any) {
+  try {
+      const res = await import(`@/mocks/course-${id}.json`);
+      return res;
+  } catch (error) {
+      console.log(error)
+  }
+}
+
+async function Chapter({course_id, chapter_id}: any) {
   console.log(course_id, chapter_id)
+
+  const DynamicCourse = getCourse(course_id);
+  const [course] = await Promise.all([DynamicCourse])
+
+  // Example usage of the chapter obj
+  console.log(course.chapters[chapter_id].answer_code)
+
   return (
     <div>
         <Instructions chapter_id={chapter_id} />
         <Editor chapter_id={chapter_id}/>
         <DiffEditor chapter_id={chapter_id}/>
-        <Sidebar course_id={course_id} chapter_id={chapter_id}/>      
+        <Sidebar course_id={course_id} chapter_id={chapter_id}/>   
     </div>
   )
 }

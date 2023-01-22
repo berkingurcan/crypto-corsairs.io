783425ebd9b3682408c37110ba00cee05e016067
berkingurcan <berkingurcan@yandex.com>
2023-01-22 20:56:35 +0300

    Update index.tsx

--- frontend/components/editor/index.tsx
+++ frontend/components/editor/index.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import Editor from "@monaco-editor/react";
 
 
-function CodeEditor({chapter_id}: any) {
+function CodeEditor({course_id, chapter_id}: any) {
+  console.log(course_id, chapter_id)
   
   return (
     <div>
-      Editor of Course: {} Chapter: {chapter_id}
+      Editor of Course: {course_id} Chapter: {chapter_id}
       <Editor
         height="90vh"
         defaultLanguage="rust"

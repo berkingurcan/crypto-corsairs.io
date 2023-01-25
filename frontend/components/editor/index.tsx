45ce8af0fb1c8ab0e64eca29181c3589b7c3dda3
berkingurcan <berkingurcan@yandex.com>
2023-01-25 13:53:22 +0300

    feat add editor theme
    see themes here: https://github.com/brijeshb42/monaco-themes/tree/master/themes

--- frontend/components/editor/index.tsx
+++ frontend/components/editor/index.tsx
@@ -1,11 +1,24 @@
 'use client';
-import React from 'react';
+import React, {useEffect} from 'react';
 import Editor from "@monaco-editor/react";
-import { DiffEditor } from '@monaco-editor/react';
+import { DiffEditor, useMonaco } from '@monaco-editor/react';
 import styles from './styles.module.css';
+import 'monaco-themes/themes/Blackboard.json';
 
 function CodeEditor({initial_code, answer_code}: any) {
-  console.log(initial_code, answer_code)
+  const monaco = useMonaco();
+
+  useEffect(() => {
+    if (monaco) {
+      console.log("here is the monaco isntance:", monaco);
+      import('monaco-themes/themes/Blackboard.json')
+        .then(data => {
+          monaco.editor.defineTheme('dracula', data);
+        })
+        .then(_ => monaco.editor.setTheme('dracula'))
+      // monaco.editor.defineTheme("monokai-bright").then(_ => monaco.editor.setMonacoTheme("monokai-bright"));
+    }
+  }, [monaco]);
 
   return (
     <div className={styles.editor}>
@@ -13,7 +26,7 @@ function CodeEditor({initial_code, answer_code}: any) {
         height="75%"
         defaultLanguage="rust"
         defaultValue={initial_code}
-        theme="vs-dark"
+        theme=""
       />
       <DiffEditor 
         height="20%"

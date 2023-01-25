92d95f39a025aa6c18b0cc1da9d2ef1017631386
berkingurcan <berkingurcan@yandex.com>
2023-01-25 14:37:25 +0300

    style remove unnecessary console.log

--- frontend/app/course/[course_id]/(chapter)/[chapter]/page.tsx
+++ frontend/app/course/[course_id]/(chapter)/[chapter]/page.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Chapter from '@/containers/chapter';
 
 function ChapterPage({params}: any) {
-  console.log(params)
   return (
     <div>
       <Chapter course_id={params.course_id} chapter_id={params.chapter} />

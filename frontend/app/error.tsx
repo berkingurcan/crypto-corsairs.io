9efefa5666cea1424bd3b557eb125a7eadf29622
berkingurcan <berkingurcan@yandex.com>
2023-01-30 16:28:02 +0300

    add loading

--- frontend/app/error.tsx
+++ frontend/app/error.tsx
@@ -4,10 +4,8 @@ import { useEffect } from 'react';
 
 export default function Error({
   error,
-  reset,
 }: {
   error: Error;
-  reset: () => void;
 }) {
   useEffect(() => {
     // Log the error to an error reporting service
@@ -17,14 +15,6 @@ export default function Error({
   return (
     <div>
       <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
     </div>
   );
 }

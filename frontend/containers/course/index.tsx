d14994f2fa2a6f40e96a198100aa4999403dac2a
berkingurcan <berkingurcan@yandex.com>
2023-02-04 15:13:51 +0300

    feat update CoursesSection fetch from net

--- frontend/containers/course/index.tsx
+++ frontend/containers/course/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import CoursesSection from '@/components/courses-section'
-import Courses from '@/mocks/courses.json';
 
-function CourseContainer() {
+async function getData() {
+  const res = await fetch(process.env.COURSES as string + `courses.json`)
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+} 
+
+async function CourseContainer() {
+  const Courses = await getData()
+
   return (
     <div>
       <CoursesSection courses={Courses}/>
